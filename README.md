9828978d83dccc9e6776c4ad46fa06580bff17fe
VeXEidolon <ariyan.zibaghamat@gmail.com>
2024-10-26 02:19:30 +0330

    new file:   README.md 	new file:   bot.js

---
+++ README.md
@@ -0,0 +1,2 @@
+# DA-Support-Bot-
+a bot made for DA that gets the contact info of a user and signs them into a website 
---
+++ bot.js
@@ -0,0 +1,29 @@
+require('dotenv').config(); // Load environment variables
+const { Telegraf } = require('telegraf');
+
+const bot = new Telegraf(process.env.TELEGRAM_API_KEY);
+
+// Set to keep track of users who have sent a message
+const users = new Set();
+
+// Start command handler
+bot.start((ctx) => {
+    ctx.reply('به پشتیبانی دنیای ارز خوش آمدید 💖 ، لطفا سوال خود را اینجا پرسیده و منتظر پاسخ تیم پشتیبانی باشید');
+    ctx.reply('لطفا صبورباشید تا پشتیبان های ما پاسخگوی شما باشند');
+});
+
+// Handle first-time message
+bot.on('text', (ctx) => {
+    const userId = ctx.from.id;
+
+    if (!users.has(userId)) {
+        users.add(userId);
+        ctx.reply('پیام شما دریافت شد لطفا تا پاسخ تیم ما شکیبا باشید 😊❤️');
+    }
+});
+
+
+
+
+
+bot.launch();
